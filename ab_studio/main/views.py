83d340d51b039ed3dccf37a0691fa28e12b8fc4c
Lasloflo <88950535+Lasloflo@users.noreply.github.com>
2021-10-19 21:12:45 +0300

    init

--- ab_studio/main/views.py
+++ ab_studio/main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+menu = {
+    'Home': 'main:home',
+    'About': 'main:about',
+    'Services': 'main:services',
+    'Gallery': 'main:gallery',
+    'Video': 'main:video',
+    'Blog': 'main:blog',
+    'Contact': 'main:contact',
+}
+
+
+def index(request):
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'main/index.html', context)
